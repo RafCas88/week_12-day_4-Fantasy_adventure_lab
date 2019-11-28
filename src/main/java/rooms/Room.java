678ce1cfc19a8a1355d34208406478c9c0165092
Raffaele Castaldo <raffaelecastaldo88@libero.it>
2019-11-28 15:29:34 +0000

    finito for now

--- src/main/java/rooms/Room.java
+++ src/main/java/rooms/Room.java
@@ -1,4 +1,23 @@
 package rooms;
 
+
+import Items.Treasure;
+
+import java.util.ArrayList;
+
 public class Room {
+
+    private ArrayList<Treasure> treasures;
+
+    public Room(){
+        this.treasures = new ArrayList<Treasure>();
+    }
+
+    public int startsEmpty(){
+        return this.treasures.size();
+    }
+
+    public void addTreasure(Treasure treasure){
+         this.treasures.add(treasure);
+    }
 }
